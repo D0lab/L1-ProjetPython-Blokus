3c29efa015039619e06c8e430d680c0c055eed68
D0lab <dolab40@gmail.com>
2024-02-10 15:15:58 +0100

    Update plateau.py

--- plateau.py
+++ plateau.py
@@ -8,31 +8,29 @@ from tkinter import Tk, Canvas
 
 
 def clic(event):
-	global c,pieces_rouge, pieces_bleu
-	item = cnv.find_closest(event.x, event.y)
-	if item[0] in pieces_rouge:
-		current_color = cnv.itemcget(item, 'fill')
-
-		if current_color == 'black':
-			cnv.itemconfig(item, fill='red')
-		else:
-			cnv.itemconfig(item, fill='black')
-	"""       
-	c = (c+1)%2
-	if ( c == 1):
+	global c,rectangle,nom_rectangle
+	
+	for rect_name, rect in pieces_rouge_loader.items():
+		coords = cnv.coords(rect)
+		if event.x >= coords[0] and event.x <= coords[2] and event.y >= coords[1] and event.y <= coords[3]:
+			rectangle = rect
+			nom_rectangle = rect_name
+	
+	
+	c = (c+1)%2 #TEST SI CLIC OU DECLIC
+	if (c == 1):
 		cnv.bind("<Motion>",glisser)
 		old[0]=event.x
 		old[1]=event.y
 	else:
 		cnv.unbind("<Motion>")
-		deposer(event.x,event.y,event)
-"""
-
-
+		deposer(event.x,event.y)
+	
 def glisser(event):
-	x1, y1, x2, y2 = cnv.coords(event)
+	global rectangle
+	x1, y1, x2, y2 = cnv.coords(rectangle)
 	if (old[0] >= x1 and old[0] <= x2 and old[1] >= y1 and old[1] <= y2):
-		cnv.move(rect, event.x-old[0], event.y-old[1])
+		cnv.move(rectangle, event.x-old[0], event.y-old[1])
 		old[0]=event.x
 		old[1]=event.y
 		
@@ -42,40 +40,40 @@ def verif_alentours(x,y):
 	print(x == 0 and y == len(plateau)-1)
 	
 	#COIN SUPERIEUR GAUCHE
-	if (x == 0 and y == 0) and (plateau[x+1][y] == 0 and plateau[x][y+1] == 0):
+	if (x == 0 and y == 0):
 		return True
 	
 	#COIN INFERIEUR GAUCHE
-	elif (x == 0 and y == len(plateau)-1) and (plateau[x+1][y] == 0 and plateau[x][y-1] == 0):
+	elif (x == 0 and y == len(plateau)-1):
 		return True
 	
 	#COIN SUPERIEUR DROIT
-	elif (x == len(plateau)-1 and y == 0) and (plateau[x-1][y] == 0 and plateau[x][y+1] == 0):
+	elif (x == len(plateau)-1 and y == 0):
 		return True
 	
 	#COIN INFERIEUR DROIT
-	elif (x == len(plateau)-1 and y == len(plateau)-1) and (plateau[x-1][y] == 0 and plateau[x][y-1] == 0):
+	elif (x == len(plateau)-1 and y == len(plateau)-1):
 		return True
+	
 	
 	#COLONNE GAUCHE
-	elif (x == 0) and (plateau[x+1][y] == 0 and plateau[x][y-1] == 0 and plateau[x][y+1] == 0) and (nb_joueurs-nb_tours != 0):
-		return True
-		
+	elif (x == 0) and (plateau[y][x+1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0) and (nb_joueurs-nb_tours != 0):
+		return True		
 	
 	#COLONNE DROITE
-	elif (x == len(plateau)-1) and (plateau[x-1][y] == 0 and plateau[x][y-1] == 0 and plateau[x][y+1] == 0) and (nb_joueurs-nb_tours != 0):
+	elif (x == len(plateau)-1) and (plateau[y][x-1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0) and (nb_joueurs-nb_tours != 0):
 		return True
 	
 	#LIGNE HAUT
-	elif (y == 0) and (plateau[x][y+1] == 0 and plateau[x-1][y] == 0 and plateau[x+1][y] == 0) and (nb_joueurs-nb_tours != 0):
+	elif (y == 0) and (plateau[y][x+1] == 0 and plateau[y][x-1] == 0 and plateau[y+1][x] == 0) and (nb_joueurs-nb_tours != 0):
 		return True
 	
 	#LIGNE BAS
-	elif (y == len(plateau)-1) and (plateau[x][y-1] == 0 and plateau[x-1][y] == 0 and plateau[x+1][y] == 0) and (nb_joueurs-nb_tours != 0):
+	elif (y == len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0) and (nb_joueurs-nb_tours != 0):
 		return True
 	
 	#TOUT LE RESTE
-	elif (plateau[x][y-1] == 0 and plateau[x][y+1] == 0 and plateau[x-1][y] == 0 and plateau[x+1][y] == 0) and (nb_joueurs-nb_tours != 0):
+	elif (x != 0 and x != len(plateau)-1 and y != 0 and y != len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0) and (nb_joueurs-nb_tours != 0):
 		return True
 		
 	else:
@@ -85,16 +83,18 @@ def afficher_plateau_console():
 	for k in plateau:
 		print(k)	
 		
-def deposer(x,y,rect):
-	global taille_plateau, plateau, nb_tours
-	x1, y1, x2, y2 = cnv.coords(rect)
+def deposer(x,y):
+	global taille_plateau,nb_tours,rectangle,nom_rectangle
+		
+	x1, y1, x2, y2 = cnv.coords(rectangle)
+	
 	move = False
 	for i in range(taille_plateau):
 		for j in range(taille_plateau):
 			if (5+i*unity <= x<= 5+(i+1)*unity and 5+j*unity <= y <= 5+(j+1)*unity):
 				print("----EMPLACEMENT CASE----",i,j)
 				if verif_alentours(i,j) == True:
-					cnv.move(rect,5+i*unity-x1,5+j*unity-y1)
+					cnv.move(rectangle,5+i*unity-x1,5+j*unity-y1)
 					plateau[j][i] = 1
 					nb_tours += 1
 					afficher_plateau_console()
@@ -102,7 +102,9 @@ def deposer(x,y,rect):
 					
 					move = True
 	if (move == False):
-		cnv.move(rect,(taille_plateau+1.05)*unity-x1,30-y1)
+	
+		coord_base = pieces_rouge_coords_base[int(nom_rectangle)]
+		cnv.move(rectangle,coord_base[0]-x1,coord_base[1]-y1)
 
 
 
@@ -112,6 +114,10 @@ old=[None, None]
 
 global c
 c = 0
+
+rect = None
+num_rect = -1
+coords_rect = None
 root = Tk()
 cnv = Canvas(root, width=1280, height=960)
 unity = 70
@@ -122,12 +128,29 @@ cnv.pack()
 
 
 plateau = []
-
 pieces_bleu = []
+
 pieces_rouge = []
+pieces_rouge_loader = {
+	"0": (cnv.create_rectangle((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity, fill='red', outline='')),
+	"1": (cnv.create_rectangle((taille_plateau+1.05)*unity, 2*unity+30, (taille_plateau+1.05)*unity+unity, 2*unity+30+unity, fill='red', outline=''))
+	}
 
+pieces_rouge_coords_base = []
+for rect_name, rect in pieces_rouge_loader.items():
+	pieces_rouge_coords_base.append(cnv.coords(rect))
+	
+"""
+pieces_rouge_loader = {
+	"0": ((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity),  # Format : (x1, y1, x2, y2)
+	"1": ((taille_plateau+1.05)*unity, 2*unity+30, (taille_plateau+1.05)*unity+unity, 2*unity+30+unity),  # Format : (x1, y1, x2, y2)
+	"2": ((taille_plateau+1.05)*unity, 3*unity+60, (taille_plateau+1.05)*unity+unity, 3*unity+60+unity),  # Format : (x1, y1, x2, y2)
+	}
 
-
+for rect_name, rect_coords in pieces_rouge_loader.items():
+	pieces_rouge.append(cnv.create_rectangle(*rect_coords, fill='red', outline='', tags=rect_name))
+"""
+	
 for i in range(taille_plateau):
 	plateau_temp = []
 	for j in range(taille_plateau):
@@ -137,16 +160,14 @@ for i in range(taille_plateau):
 
 		
 
-rect1 = cnv.create_rectangle((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity, fill='red', outline='')
-pieces_rouge.append(rect1)
-rect2 = cnv.create_rectangle((taille_plateau+1.05)*unity, 2*30+unity, (taille_plateau+1.05)*unity+unity, (2*30+unity)+unity, fill='red', outline='')
-pieces_rouge.append(rect2)
+
+#cnv.create_rectangle((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity, fill='red', outline='')
+
 for i in range(taille_plateau+1):
 	cnv.create_line(5,5+unity*i,(taille_plateau+0.05)*unity,5+unity*i)
 	cnv.create_line(5+unity*i,5,5+unity*i,(taille_plateau+0.05)*unity)
 
 
 cnv.bind("<Button-1>",clic)
-
 
 root.mainloop()
