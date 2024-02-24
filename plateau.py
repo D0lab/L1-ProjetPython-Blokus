a25734a97bcb3f89cc01c8111673efa5bfcbe65d
D0lab <dolab40@gmail.com>
2024-02-24 16:34:23 +0100

    Update plateau.py

--- plateau.py
+++ plateau.py
@@ -47,63 +47,67 @@ def verif_alentours(x,y):
 	liste_indicatif = [0,1,2]
 	liste_indicatif.pop(1)
 	print("---",x,y)
-	
-	
-	#COIN SUPERIEUR GAUCHE
-	if (x == 0 and y == 0):
-		return 1
-	
-	#COIN INFERIEUR GAUCHE
-	elif (x == 0 and y == len(plateau)-1):
-		return 1
-	
-	#COIN SUPERIEUR DROIT
-	elif (x == len(plateau)-1 and y == 0) and (plateau[y][x] == 0):
-		return 1
-	
-	#COIN INFERIEUR DROIT
-	elif (x == len(plateau)-1 and y == len(plateau)-1):
-		return 1
-	
-	
-	#COLONNE GAUCHE
-	elif (y != 0 and y != len(plateau)-1) and (x == 0) and (plateau[y][x+1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
-		if (plateau[y+1][x+1] != 0 or plateau[y-1][x+1] != 0):
-			return 1
-		else:
-			return 0
-	
-	#COLONNE DROITE
-	elif (y != 0 and y != len(plateau)-1) and (x == len(plateau)-1) and (plateau[y][x-1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
-		if (plateau[y+1][x-1] != 0 or plateau[y-1][x-1] != 0):
-			return 1
-		else:
-			return 0
-	
-	#LIGNE HAUT 
-	
-	elif (x != 0 and x != len(plateau)-1) and (y == 0) and (plateau[y][x+1] == 0 and plateau[y][x-1] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):		
-		if (plateau[y+1][x+1] != 0 or plateau[y+1][x-1] != 0):
-			return 1
-		else:
-			return 0
-	
-	
-	#LIGNE BAS
-	elif (x != 0 and x != len(plateau)-1) and (y == len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
-		if (plateau[y-1][x+1] != 0 or plateau[y-1][x-1] != 0):
-			return 1
-		else:
-			return 0
 
-	#TOUT LE RESTE
-	elif (x != 0 and x != len(plateau)-1 and y != 0 and y != len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
-		if (plateau[y+1][x-1] != 0 or plateau[y+1][x+1] != 0 or plateau[y-1][x-1] != 0 or plateau[y-1][x+1] != 0):
+	try:
+	
+	
+		#COIN SUPERIEUR GAUCHE
+		if (x == 0 and y == 0):
 			return 1
-		else:
-			return 0
 		
-	else:
+		#COIN INFERIEUR GAUCHE
+		elif (x == 0 and y == len(plateau)-1):
+			return 1
+		
+		#COIN SUPERIEUR DROIT
+		elif (x == len(plateau)-1 and y == 0) and (plateau[y][x] == 0):
+			return 1
+		
+		#COIN INFERIEUR DROIT
+		elif (x == len(plateau)-1 and y == len(plateau)-1):
+			return 1
+		
+		
+		#COLONNE GAUCHE
+		elif (y != 0 and y != len(plateau)-1) and (x == 0) and (plateau[y][x+1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
+			if (plateau[y+1][x+1] != 0 or plateau[y-1][x+1] != 0):
+				return 1
+			else:
+				return 0
+		
+		#COLONNE DROITE
+		elif (y != 0 and y != len(plateau)-1) and (x == len(plateau)-1) and (plateau[y][x-1] == 0 and plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
+			if (plateau[y+1][x-1] != 0 or plateau[y-1][x-1] != 0):
+				return 1
+			else:
+				return 0
+		
+		#LIGNE HAUT 
+		
+		elif (x != 0 and x != len(plateau)-1) and (y == 0) and (plateau[y][x+1] == 0 and plateau[y][x-1] == 0 and plateau[y+1][x] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):		
+			if (plateau[y+1][x+1] != 0 or plateau[y+1][x-1] != 0):
+				return 1
+			else:
+				return 0
+		
+		
+		#LIGNE BAS
+		elif (x != 0 and x != len(plateau)-1) and (y == len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
+			if (plateau[y-1][x+1] != 0 or plateau[y-1][x-1] != 0):
+				return 1
+			else:
+				return 0
+
+		#TOUT LE RESTE
+		elif (x != 0 and x != len(plateau)-1 and y != 0 and y != len(plateau)-1) and (plateau[y-1][x] == 0 and plateau[y+1][x] == 0 and plateau[y][x-1] == 0 and plateau[y][x+1] == 0 and plateau[y][x] == 0) and (nb_joueurs-nb_tours != 0):
+			if (plateau[y+1][x-1] != 0 or plateau[y+1][x+1] != 0 or plateau[y-1][x-1] != 0 or plateau[y-1][x+1] != 0):
+				return 1
+			else:
+				return 0
+			
+		else:
+			return -1
+	except:
 		return -1
 
 def afficher_plateau_console():
@@ -134,8 +138,9 @@ def deposer(x,y):
 					print("tag: ",tag)
 					print(verif_alentours(int(tag[0]//unity),int(tag[1]//unity)))
 					total_code += verif_alentours(int(tag[0]//unity),int(tag[1]//unity))
-					if total_code > 0:
-						flag_verif_boucle = False
+					
+				if total_code <= 0:
+					flag_verif_boucle = False
 						
 
 				if flag_verif_boucle:
@@ -144,7 +149,7 @@ def deposer(x,y):
 					for tag in tagged_rectangles:
 						plateau[int(tag[1]//unity)][int(tag[0]//unity)] = 1
 					nb_tours += 1
-					#afficher_plateau_console()
+					afficher_plateau_console()
 					move = True
 
 					flag_estSuppr = False
@@ -183,8 +188,10 @@ num_rect = -1
 coords_rect = None
 flag_pose = 0
 
-unity = 70
+unity = 60
 taille_plateau = 12
+decalage=1280/2-unity*taille_plateau//2
+
 nb_joueurs = 1
 nb_tours = 1
 mute_son = 0
@@ -205,7 +212,7 @@ son_placement_piece = "sons/lego_build.wav"
 
 
 def build_pieces_rouge():
-	global pieces_rouge_loader, nb_pieces_rouge, pieces_rouge_coords_base
+	global pieces_rouge_loader, nb_pieces_rouge, pieces_rouge_coords_base,decalage
 	# pieces_rouge_loader = {
 	# 	#"NUMERO-X-Y :" (cnv.create_rectangle((x1, y1, x2, y2, fill='couleur', outline='', tags="rectNUM_RECT"))
 	# 	"0-0-0": (cnv.create_rectangle((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity, fill='red', outline='', tags="rect0")),
@@ -245,12 +252,14 @@ def build_plateau():
 		
 	#cnv.create_rectangle((taille_plateau+1.05)*unity, 30, (taille_plateau+1.05)*unity+unity, 30+unity, fill='red', outline='')
 
+	
 	for i in range(taille_plateau+1):
-		cnv.create_line(5,5+unity*i,(taille_plateau+0.05)*unity,5+unity*i)
-		cnv.create_line(5+unity*i,5,5+unity*i,(taille_plateau+0.05)*unity)
+		cnv.create_line(5+decalage,5+unity*i,(taille_plateau+0.05)*unity+decalage,5+unity*i)
+		cnv.create_line(5+unity*i+decalage,5,5+unity*i+decalage,(taille_plateau+0.05)*unity)
 
-build_plateau()
 build_pieces_rouge()
+build_plateau()
+
 
 
 
