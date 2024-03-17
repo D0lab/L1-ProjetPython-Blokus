220f3b967bb3d337eb7af2d53bf7cb5a3fb4142b
D0lab <dolab40@gmail.com>
2024-03-17 22:56:21 +0100

    e

---
+++ LOCAL - MATYS/controller.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+@author: mlaurent,dlabaste
+Plateau de jeu Blokus
+"""
+
+from ihm import *
+
+charger_menu()
+#charger_root()
+
+
+
+
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
