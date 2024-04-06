d4f473cb4e7aaee122eae95dd6260966bb9f8b23
SyMat12 <matys12laurent@gmail.com>
2024-04-06 20:40:58 +0200

    cdqcazfdsfsjhgtj,dyhj hg,dh,h654n1d65n13fgndfg6n516hgf54nfg5n1

---
+++ LOCAL - MATYS/controller.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+@author: mlaurent,dlabaste
+Plateau de jeu Blokus
+"""
+
+from ihm import *
+
+charger_menu()
+# charger_root()
+# charger_final([10,8,10,10])
+
+
+
+
+
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
