83fef50452ee47426ae9745346d6ab40a8d83dcc
D0lab <dolab40@gmail.com>
2024-04-07 19:44:19 +0200

    vfinal

---
+++ controller.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+@author: mlaurent,dlabaste
+controller de jeu Blokus
+"""
+
+from ihm import *
+
+#LANCER MENU
+charger_menu()
+
+#LANCER PARAMETRES
+# charger_settings("menu")
+
+#LANCER CREDITS
+# charger_credits()
+
+#LANCER ECRAN DE FIN ([[score_j1,0][score_j2,1][score_j3,2][score_j4,3]])
+# charger_final([[10,0][9,1][8,2][7,3]])
+
+
+
+
+
+
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
