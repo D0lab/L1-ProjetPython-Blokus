d779b40701874a4913b9dfb628437e3a22dcb7b0
D0lab <dolab40@gmail.com>
2024-04-07 19:52:13 +0200

    final

---
+++ install_bibli.bat
@@ -0,0 +1,23 @@
+chcp 65001
+
+@echo off
+echo Installation des bibliothèques Python avec pip...
+
+rem Installer tkinter
+pip install tk
+
+rem Installer customtkinter
+pip install customtkinter
+
+rem Installer tkextrafont
+pip install tkextrafont
+
+rem Installer pygame
+pip install pygame
+
+rem Installer pillow
+pip install pillow
+
+echo Installation des bibliothèques Python terminée.
+echo.
+pause
